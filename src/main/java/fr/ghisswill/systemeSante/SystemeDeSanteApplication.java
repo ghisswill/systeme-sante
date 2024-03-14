482abeb719dc9af0e926e19b942057e42be96282
« ghisswill» <kkghislain@hotmail.fr>
2024-03-14 21:51:06 +0100

    initialisation du projet git

---
+++ src/main/java/fr/ghisswill/systemeSante/SystemeDeSanteApplication.java
@@ -0,0 +1,13 @@
+package fr.ghisswill.systemeSante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeDeSanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeDeSanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ghisswill/systemeSante/SystemeDeSanteApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ghisswill.systemeSante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeDeSanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
